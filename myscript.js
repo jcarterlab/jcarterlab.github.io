33b6e03113d6f8ea00b23e0b84df35d1e89726b7
Jack Carter <jack-p-carter@outlook.com>
2023-01-05 20:32:28 +0000

    testing

--- myscript.js
+++ myscript.js
@@ -33,3 +33,31 @@ function hide_text(c) {
 		x.style.display = "none";
 	}
 }
+
+
+
+
+let slideIndex = [1,1];
+let slideId = ["algorithm_slides", "migration_slides"];
+let dotId = ["algorithm_dots", "migration_dots"];
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  let dots = document.getElementsByClassName(dotId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+	  dots[i].className = dots[i].className.replace("active", dotId[no]);
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+  dots[slideIndex[no]-1].className += " active";
+}
