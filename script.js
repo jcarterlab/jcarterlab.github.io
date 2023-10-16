8fd56b5141fca4fac452acc7d06f77134d8eea5a
Jack Carter <jack-p-carter@outlook.com>
2023-10-16 22:01:28 +0100

    updating files

--- script.js
+++ script.js
@@ -15,19 +15,20 @@
         }
 
     /* next/previous buttons for photography */
-    var images = ["https://jcarterlab.github.io/Images/Photography/Luxor.JPG", 
+    var images = [ 
     "https://jcarterlab.github.io/Images/Photography/Mardin.JPG", 
     "https://jcarterlab.github.io/Images/Photography/Egirdir.JPG", 
     "https://jcarterlab.github.io/Images/Photography/Istanbul.JPG",
     "https://jcarterlab.github.io/Images/Photography/Kars.JPG", 
-    "https://jcarterlab.github.io/Images/Photography/Cappadocia.JPG", 
-    "https://jcarterlab.github.io/Images/Photography/Kotor.JPG", 
-    "https://jcarterlab.github.io/Images/Photography/SanTorini.JPG",
-    "https://jcarterlab.github.io/Images/Photography/Meteora.JPG", 
-    "https://jcarterlab.github.io/Images/Photography/Madrid.JPG", 
-    "https://jcarterlab.github.io/Images/Photography/SiemReap.JPG", 
-    "https://jcarterlab.github.io/Images/Photography/HoiAn.JPG",
-    "https://jcarterlab.github.io/Images/Photography/Singapore.JPG"];
+    "https://jcarterlab.github.io/Images/Photography/Cappadocia.JPG",
+    ];
+    var alts = [
+        "Mardin",
+        "Egirdir",
+        "Istanbul",
+        "Kars",
+        "Cappadocia",
+    ]
     var currentImageIndex = 0;
     var imageElement = document.getElementById('image');
 
@@ -43,4 +44,5 @@
     function previousImage() {
         currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
         imageElement.src = images[currentImageIndex];
+        imageElement.alt = alts[currentImageIndex];
     }
